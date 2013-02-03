b0afb24d60d8d12b69ad959592f4a4106498e8fe
yueyoum <yueyoum@gmail.com>
2013-02-03 23:26:26 +0800

    check whether i3status is running

--- i3dzen2.sh
+++ i3dzen2.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+running=`ps -ef | grep i3status | grep -v grep | wc -l`
+[[ $running == 1 ]] && exit 1
+
+
 SELF_PATH=`readlink -f $0`
 PROJECT_PATH=`dirname "$SELF_PATH"`
 
 cd "$PROJECT_PATH"
 
-sleep 1
+sleep 3
 i3status | ./i3-dzen-bridge.py | dzen2 -y -1 -fn "ubuntu mono" -ta r &
 
 exit 0
